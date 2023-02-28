7a467faeabd6b1fe85a6486356b4675073fc6b2d
Jan Janech <jan.janech@fri.uniza.sk>
2023-02-28 15:07:49 +0100

    02 - refaktoring - zmena switch na pouzitie predmetu na polymorfizmus

---
+++ wof/src/sk/uniza/fri/wof/prostredie/ObycajnyPredmet.java
@@ -0,0 +1,19 @@
+package sk.uniza.fri.wof.prostredie;
+
+public class ObycajnyPredmet implements Predmet {
+    private final String nazov;
+
+    public ObycajnyPredmet(String nazov) {
+        this.nazov = nazov;
+    }
+
+    @Override
+    public String getNazov() {
+        return this.nazov;
+    }
+
+    @Override
+    public void pouzi() {
+        System.out.format("Predmet %s sa neda pouzit!%n", this.nazov);
+    }
+}
--- wof/src/sk/uniza/fri/wof/prostredie/Predmet.java
+++ wof/src/sk/uniza/fri/wof/prostredie/Predmet.java
@@ -1,39 +1,7 @@
 package sk.uniza.fri.wof.prostredie;
 
-import sk.uniza.fri.wof.zaklad.Hrac;
+public interface Predmet {
+    String getNazov();
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class Predmet {
-    private final String nazov;
-    private boolean navlekySuObute;
-
-    public Predmet(String nazov) {
-        this.nazov = nazov;
-    }
-
-    public String getNazov() {
-        return this.nazov;
-    }
-
-    public void pouzi() {
-        switch (this.nazov) {
-            case "hodinky" -> {
-                String timeStamp = new SimpleDateFormat("HH:mm:ss").format(new Date());
-                System.out.printf("Aktualny cas je %s%n", timeStamp);
-            }
-            case "navleky" -> {
-                if (this.navlekySuObute) {
-                    System.out.println("Vyzul si si navleky");
-                } else {
-                    System.out.println("Obul si si navleky");
-                }
-                this.navlekySuObute = !this.navlekySuObute;
-            }
-            default -> {
-                System.out.format("Predmet %s sa neda pouzit!%n", this.nazov);
-            }
-        }
-    }
+    void pouzi();
 }
